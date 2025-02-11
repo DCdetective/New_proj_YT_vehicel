281b94cf13746e3e52d7314a750f1a5d0ede4db0
DCdetective <superaziz1234@gmail.com>
2025-02-11 10:31:24 +0530

    Exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from src.logger import logging
 
 logging.debug("This is a debug message")
+
+from src.exception import MyException
+import sys
+try:
+    a = 1/0
+except Exception as e:
+    raise MyException(e,sys) from e
